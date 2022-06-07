61b571f8b94d54a21a5ec70732c0963fe76656fe
Nikita Miroshnichenko <nikitamiroshnichenko@coherentsolutions.com>
2022-06-08 01:19:04 +0200

    created api interface, implemented dto classes and models, updated gradle

---
+++ app/src/main/java/com/mirosha/cryptocurrencyapp/data/remote/dto/coin_detail/LinksExtended.kt
@@ -0,0 +1,7 @@
+package com.mirosha.cryptocurrencyapp.data.remote.dto.coin_detail
+
+data class LinksExtended(
+    val stats: Stats,
+    val type: String,
+    val url: String
+)
---
+++ app/src/main/java/com/mirosha/cryptocurrencyapp/data/remote/dto/coin_detail/WhitePaper.kt
@@ -0,0 +1,6 @@
+package com.mirosha.cryptocurrencyapp.data.remote.dto.coin_detail
+
+data class WhitePaper(
+    val link: String,
+    val thumbnail: String
+)
