11c65b517ee3eaf65524b411ec8902ca854343bc
Nikita Miroshnichenko <nikitamiroshnichenko@coherentsolutions.com>
2022-06-08 00:07:18 +0200

    changed compose theming

--- app/src/main/java/com/mirosha/cryptocurrencyapp/ui/theme/Theme.kt
+++ app/src/main/java/com/mirosha/cryptocurrencyapp/ui/theme/Theme.kt
@@ -1,35 +1,27 @@
 package com.mirosha.cryptocurrencyapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = ColorPrimary,
+    background = DefaultWhite,
+    onBackground = MediumGray,
+    onPrimary = DarkGray
 )
 
 @Composable
-fun CryptocurrencyAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+fun AppTheme(
+    darkTheme: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
