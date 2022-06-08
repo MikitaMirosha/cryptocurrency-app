bf7e86ac411c3b7dbe7596a62d6e67c245b5adfc
Nikita Miroshnichenko <nikitamiroshnichenko@coherentsolutions.com>
2022-06-08 20:59:07 +0200

    implemented app module

---
+++ app/src/main/java/com/mirosha/cryptocurrencyapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.mirosha.cryptocurrencyapp.di
+
+import com.mirosha.cryptocurrencyapp.common.constants.ApiConstants.BASE_URL
+import com.mirosha.cryptocurrencyapp.data.remote.CoinPaprikaApi
+import com.mirosha.cryptocurrencyapp.data.repository.CoinRepositoryImpl
+import com.mirosha.cryptocurrencyapp.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
